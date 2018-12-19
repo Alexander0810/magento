d24ae862937300e9a74d356687fbc39d1bc26896
Alexander Shutalyov <alexander.sh0810@gmail.com>
2018-12-19 20:09:50 +0200

    grid finish

--- Grid/view/frontend/web/js/grid.js
+++ Grid/view/frontend/web/js/grid.js
@@ -1,9 +1,5 @@
 
 define(['uiComponent', 'jquery', 'ko', 'Magento_Ui/js/modal/modal'], function(ui, $, ko, modal){
-
-	return ui.extend({
-    	
-		reserve: function() {
 				var options = {
 		        type: 'popup', // popup or slide
         		responsive: true, // true = on smaller screens the modal slides in from the right
@@ -15,36 +11,41 @@ define(['uiComponent', 'jquery', 'ko', 'Magento_Ui/js/modal/modal'], function(ui
            				var username = document.getElementById('input_name').value;
 						var userphone = document.getElementById('input_phone').value;
 
-		            				$.ajax({
-										url: 'rest/V1/alexander_grid_save',
-										method: 'POST',
-										contentType: 'application/json',
-										data: JSON.stringify({"name" : username , "phone" : userphone }),
-										success: function(response) {
-											response = JSON.parse(response);
-										},
-										error: function() {
-											//
-										}
-									})
-		                    this.closeModal(); // This button closes the modal
-		                }
-          		  	},
+	            				$.ajax({
+									url: 'rest/V1/alexander_grid_save',
+									method: 'POST',
+									contentType: 'application/json',
+									data: JSON.stringify({"name" : username , "phone" : userphone }),
+									success: function(response) {
+										alert('Request sent');
+										response = JSON.parse(response);
+									},
+									error: function() {
+										alert('Request was not sent');
+										//
+									}
+								})
+			                    this.closeModal(); // This button closes the modal
+			                }
+	          		  	},
 
-          		  	{ // Add array of buttons within the modal if you need.
-		            text: $.mage.__('Cancel'),
-		            class: '',
-		            click: function () {
+          		  		{ // Add array of buttons within the modal if you need.
+			    	        text: $.mage.__('Cancel'),
+				            class: '',
+			        	    click: function () {
 		                    this.closeModal(); // This button closes the modal
 		                }
           		  	}]
      			};
-
-		        var popup = modal(options, $('#modal-content'));
-
-		        $("#modal-btn").click(function() {
+	return ui.extend({
+    	
+		reserve: function() {
 		            $('#modal-content').modal('openModal');
-		        });
+	   	},
+
+	   	isVisible: function() {
+		    var popup = modal(options, $('#modal-content'));
+  		   	return true;
 	   	},
 
 		initialize: function() {					
